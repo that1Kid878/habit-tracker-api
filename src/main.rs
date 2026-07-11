6cf9620759b696cbaf6690d82eabb3fcaab9a476
that1Kid <a.anaqi2010@gmail.com>
2026-07-11 23:07:09 +0800

    Init: Project

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use axum::Router;
+use tokio::net::TcpListener;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let app = Router::new();
+    let listener = TcpListener::bind("0.0.0.0:8080").await?;
+    println!("App has been set up");
+
+    axum::serve(listener, app).await?;
+    Ok(())
+}
