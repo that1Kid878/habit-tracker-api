f0867af9a813196d34116066ef22b8e57faf8add
that1Kid <a.anaqi2010@gmail.com>
2026-07-17 11:48:07 +0800

    Add: Make sqlx send http errors

---
+++ src/errors.rs
@@ -0,0 +1,29 @@
+use sqlx::error::ErrorKind;
+
+use crate::responses::AppError;
+
+impl From<sqlx::Error> for AppError {
+    fn from(err: sqlx::Error) -> Self {
+        match err {
+            sqlx::Error::RowNotFound => AppError::NotFound {
+                message: format!("Row was not found: {}", err),
+            },
+
+            sqlx::Error::Database(db_err) => match db_err.kind() {
+                ErrorKind::NotNullViolation => AppError::BadRequest {
+                    message: format!("Not null constraint violation: {}", db_err.message()),
+                },
+                ErrorKind::UniqueViolation => AppError::BadRequest {
+                    message: format!("Unique constraint violation: {}", db_err.message()),
+                },
+                _ => AppError::InternalServerError {
+                    message: format!("Database Error: {}", db_err.message()),
+                },
+            },
+
+            _ => AppError::InternalServerError {
+                message: format!("SQLx Error: {}", err),
+            },
+        }
+    }
+}
